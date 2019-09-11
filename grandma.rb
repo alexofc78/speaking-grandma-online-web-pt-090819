b6441dbe2db98a1ae6c875747234a17c74e8ffbc
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-11 15:10:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(greeting)
-  if ("I LOVE YOU GRANDMA".upcase)
+  if greeting == "I LOVE YOU GRANDMA".upcase
     return "I LOVE YOU TOO PUMPKIN!".upcase
   else 
     return "HUH?! SPEAK UP, SONNY!".upcase
